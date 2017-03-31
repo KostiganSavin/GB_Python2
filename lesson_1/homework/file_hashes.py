6cc816cd50fd313594e86d989fea2e9bf3070ce0
Konsatntin Savin <kostiga.savin@gmail.com>
2017-03-31 15:50:03 +0300

    Реализовал алгоритм в виде функции склеивания файла из фрагментов на основании значений ХЭШ фукции фрагментов

--- lesson_1/homework/file_hashes.py
+++ lesson_1/homework/file_hashes.py
@@ -5,30 +5,44 @@ import hashlib
 FILE_NAME_WITH_PARTS = 'parts.md5'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 print(BASE_DIR)
-list_files = os.listdir(os.path.join(BASE_DIR, 'files/file2'))
-_files = dict()
-
-for file in list_files:
-    if not (file == FILE_NAME_WITH_PARTS):
-        # print(file)
-        with open(os.path.join(BASE_DIR, 'files/file2', file), 'rb') as f:
-            b = f.read()
-            h = hashlib.md5(b)
-            # print(h.hexdigest())
-        _files[h.hexdigest()] = file
-
-# print(_files)
-
-file_with_hash = os.path.join(BASE_DIR, 'files/file2', FILE_NAME_WITH_PARTS)
-with open(file_with_hash) as file_hashes:
-    for line in file_hashes:
-        print(_files[line.strip()])
-        with open(os.path.join(BASE_DIR, 'files/file2', 'out_file'), 'ab') as out_file:
-             f = open(os.path.join(BASE_DIR, 'files/file2', _files[line.strip()]), 'rb').read()
-             out_file.write(f)
 
 
+def concat_file(dirname, file_hashes, out_file):
+    """
+    Функция соединяющая частички файла в единый файл
+    на основании содержания фала с ХЭШ
 
-# print(lst)
-# for item in lst:
-# print(_files[item])
+    """
+    list_files = os.listdir(os.path.join(BASE_DIR, 'files', dirname))
+    _files = dict()
+
+    for file in list_files:
+        if not (file == file_hashes):
+            with open(os.path.join(BASE_DIR, 'files',
+                                   dirname, file), 'rb') as f:
+                b = f.read()
+                h = hashlib.md5(b)
+            _files[h.hexdigest()] = file
+    file_with_hash = os.path.join(BASE_DIR, 'files',
+                                  dirname, file_hashes)
+    with open(file_with_hash) as file_hash:
+        for line in file_hash:
+            print(_files[line.strip()])
+            with open(os.path.join(BASE_DIR, 'files', dirname,
+                                   out_file), 'ab') as out_f:
+                f = open(os.path.join(BASE_DIR, 'files', dirname,
+                                      _files[line.strip()]), 'rb').read()
+                out_f.write(f)
+
+
+def split_file(file, size):
+    pass
+
+
+def count_hash(dirname, name_hasgfile):
+    pass
+
+
+if __name__ == '__main__':
+    # concat_file('file1', 'parts.md5', 'result_file')
+    concat_file('file2', 'parts.md5', 'result_file')
