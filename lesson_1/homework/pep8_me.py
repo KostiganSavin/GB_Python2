a272c032e7811ac311dea6e5979e24853ae30fde
Konsatntin Savin <kostiga.savin@gmail.com>
2017-03-31 16:10:01 +0300

    Привел файл к окончательному варианту по PEP8

--- lesson_1/homework/pep8_me.py
+++ lesson_1/homework/pep8_me.py
@@ -3,20 +3,20 @@ import string
 
 
 def create_file(namef, dir, size):
-    if(size.isdigit() is not True):
+    if size.isdigit():
         if size.endswith('KB'):
             s1 = size.split('KB')
             size1 = int(s1[0])*1024
             token = ''.join(random.choice(string.ascii_uppercase +
                             string.ascii_lowercase + string.digits)
                             for x in range(size1))
-        if size.endswith('MB') is True:
+        if size.endswith('MB'):
             s1 = size.split('MB')
             size1 = int(s1[0])*1048567
             token = ''.join(random.choice(string.ascii_uppercase +
                             string.ascii_lowercase + string.digits)
                             for x in range(size1))
-        if size.endswith('GB') is True:
+        if size.endswith('GB'):
             s1 = size.split('GB')
             size1 = int(s1[0]) * 1073741824
             token = ''.join(random.choice(string.ascii_uppercase +
@@ -26,7 +26,7 @@ def create_file(namef, dir, size):
         token = ''.join(random.choice(string.ascii_uppercase +
                         string.ascii_lowercase + string.digits)
                         for x in range(int(size)))
-    file = open(dir+namef, "w")
+    file = open(dir + namef, "w")
     file.write(token)
 
 
