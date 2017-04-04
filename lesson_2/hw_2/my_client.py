844a11299c5b72ba1cecc343f6d773d24acdec7e
Konsatntin Savin <kostiga.savin@gmail.com>
2017-04-04 16:29:42 +0300

    Переписал реализацию сервера и клиента под Python3.5

--- lesson_2/hw_2/my_client.py
+++ lesson_2/hw_2/my_client.py
@@ -3,10 +3,10 @@ import socket
 HOST, PORT = 'localhost', 9999
 data = input('Введите сообщение для сервера:')
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + '\n', 'utf-8'))
-    recived = str(sock.recv(1024), 'utf-8')
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+sock.sendall(bytes(data + '\n', 'utf-8'))
+recived = str(sock.recv(1024), 'utf-8')
 
 print('Отправлено:{}'.format(data))
 print('Получено:  {}'.format(recived))
--- lesson_2/hw_2/my_server.py
+++ lesson_2/hw_2/my_server.py
@@ -1,16 +1,17 @@
 import socketserver
 
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
-    
     def handle(self):
         self.data = self.request.recv(1024).decode()
         print('Клиент {} сообщает {}'.format(self.client_address[0], self.data))
         self.request.sendall(bytes(self.data.upper(), 'utf-8'))
-       
+
 
 if __name__ == '__main__':
     HOST, PORT = 'localhost', 9999
 
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        print('Сервер запущен')
-        server.serve_forever()
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    print('Сервер запущен')
+
+    server.serve_forever()
