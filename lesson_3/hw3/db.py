999b483dbac7858b23fc0dad7de5042dda87aeab
Konstantin Savin <kostigan.savin@gmail.com>
2017-04-07 20:10:05 +0300

    Добавил наброски для запросов к БД

--- lesson_3/hw3/db.py
+++ lesson_3/hw3/db.py
@@ -55,13 +55,52 @@ def make_db(clear=None):
 
 class DbWrite:
 
-    def write_to_terminal():
+    def write_to_terminal(configuration, title, comment, pub_key):
         with sqlite3.connect('company.db3') as conn:
             cursor = conn.cursor()
             cursor.execute('''insert into terminal
-                            (`configuration`, `title`, `comment`, `pub_key`)
-                            values(?, ?, ?, ?);'''())
+                           (`configuration`, `title`, `comment`, `pub_key`)
+                           values(?, ?, ?, ?);''',
+                           (configuration, title, comment, pub_key))
 
+    def write_to_partner(title, comment):
+        with sqlite3.connect('company.db3') as conn:
+            cursor = conn.cursor()
+            cursor.execute('''insert into partner
+                           (`configuration`, `title`, `comment`, `pub_key`)
+                           values(?, ?);''',
+                           (title, comment))
+
+    def write_to_payment(datetime, terminal_id,
+                         transaction_id, partner_id, summ):
+        with sqlite3.connect('company.db3') as conn:
+            cursor = conn.cursor()
+            cursor.execute('''insert into payment
+                           (`datetime`, `terminal_id`, `transaction_id`,
+                            `partner_id`, `summ`) values(?, ?, ?, ?, ?);''',
+                           (datetime, terminal_id, transaction_id,
+                            partner_id, summ))
+
+
+def get_transaction_for_term_in_period(terminal_id, start_date, end_date):
+    with sqlite3.connect('company.db3') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''''')
+    pass
+
+
+def get_sum_for_partners_in_period(start_date, end_date):
+    with sqlite3.connect('company.db3') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''''')
+    pass
+
+
+def get_sum_for_each_term_in_period(start_date, end_date):
+    with sqlite3.connect('company.db3') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''''')
+    pass
 
 
 def main():
