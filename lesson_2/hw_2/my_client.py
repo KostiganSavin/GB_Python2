2b4095df3792cd51b8dfc53065b179da7cf8d119
Konstantin Savin <kostigan.savin@gmail.com>
2017-04-07 09:40:56 +0300

    Сделал наброски для бинарного протокола.

--- lesson_2/hw_2/my_client.py
+++ lesson_2/hw_2/my_client.py
@@ -1,12 +1,34 @@
 import socket
+import struct
+import datetime
+from collections import namedtuple
 
 HOST, PORT = 'localhost', 9999
-data = input('Введите сообщение для сервера:')
+PACKET_HEAD = b'zz'
+# data = input('Введите сообщение для сервера:')
+
+
+def date_encode(year, month, day):
+    year -= 2000
+    date = (year << 9) | (month << 5) | (day & 31)
+    return date
+
+
+d = datetime.date(2017, 4, 3)
+date = date_encode(d.year, d.month, d.day)
+
+data = struct.pack('2si', PACKET_HEAD, date,)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
-sock.sendall(bytes(data + '\n', 'utf-8'))
-recived = str(sock.recv(1024), 'utf-8')
+# sock.sendall(bytes(data + '\n', 'utf-8'))
+sock.sendall(data)
+# recived = str(sock.recv(1024), 'utf-8')
+recived = sock.recv(1024)
+
 
 print('Отправлено:{}'.format(data))
 print('Получено:  {}'.format(recived))
+
+data = struct.unpack('2si', recived)
+print('Распаковано:', data)
--- lesson_2/hw_2/my_server.py
+++ lesson_2/hw_2/my_server.py
@@ -3,10 +3,11 @@ import socketserver
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        self.data = self.request.recv(1024).decode()
+        # self.data = self.request.recv(1024).decode()
+        self.data = self.request.recv(1024)
         print('Клиент {} сообщает {}'.format(self.client_address[0], self.data))
-        self.request.sendall(bytes(self.data.upper(), 'utf-8'))
-
+        # self.request.sendall(bytes(self.data.upper(), 'utf-8'))
+        self.request.sendall(self.data)
 
 if __name__ == '__main__':
     HOST, PORT = 'localhost', 9999
