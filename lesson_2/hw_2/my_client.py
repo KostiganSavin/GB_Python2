8b0466f1776bf30697ca877dbb4f3d052f439d4d
Konstantin Savin <kostigan.savin@gmail.com>
2017-04-10 22:19:24 +0300

    Добавил кодирование времени в 3 байта. Сделал декодирование пакета.

--- lesson_2/hw_2/my_client.py
+++ lesson_2/hw_2/my_client.py
@@ -6,23 +6,27 @@ from collections import namedtuple
 HOST, PORT = 'localhost', 9999
 PACKET_HEAD = b'zz'
 # data = input('Введите сообщение для сервера:')
-ServiceTransaction = namedtuple('ServiceTransaction', ('''head, lenght,
-                                 term_id, transaction_id, date, time,
-                                 transaction_type, data'''))
+ServiceTransaction = namedtuple('ServiceTransaction', ('''lenght,
+                                 term_id, transaction_id, date, time_1,
+                                 time_2, time_3, transaction_type, data'''))
 
-PayTransaction = namedtuple('PayTransaction', ('''head, lenght,
-                                 term_id, transaction_id, date, time,
-                                 transaction_type, organization_id, summ'''))
-
-InkassTransaction = namedtuple('InkassTransaction', ('''head, lenght,
-                                 term_id, transaction_id, date, time,
-                                 transaction_type, person_id, summ'''))
+PayTransaction = namedtuple('PayTransaction', ('''lenght,
+                                 term_id, transaction_id, date, time_1,
+                                 time_2, time_3, transaction_type,
+                                 id_, summ'''))
 
 
 def date_encode(year, month, day):
     year -= 2000
     date = (year << 9) | (month << 5) | (day & 31)
     return date
+
+
+def time_encode(sec):
+    first = ((sec & 0xff0000) >> 16)
+    second = ((sec & 0xff00) >> 8)
+    third = (sec & 0xff)
+    return (first, second, third)
 
 
 def make_packet(data):
@@ -43,27 +47,30 @@ def make_packet(data):
 
 d = datetime.date(2017, 4, 3)
 date = date_encode(d.year, d.month, d.day)
-t = (20012 & 0xffffff)
+t = time_encode(20012)
 
-st = ServiceTransaction(b'zz', 12, 1, 1, date, t, 0, 0) 
-pt = PayTransaction(b'zz', 24, 1, 1, date, t, 1, 101, 20000)
-it = InkassTransaction(b'zz', 24, 1, 1, date, t, 2, 102, 20000)
-t_lst = [st, pt, it]
+lst = [13, 1, 1, date]
+lst.extend(t)
+lst.extend([0, 2])
+st = ServiceTransaction(*lst)
+st_pack = struct.Struct('BHIH3B2B')
+packet = st_pack.pack(*st)
+packet = PACKET_HEAD + packet
+print(packet)
+# pt = PayTransaction(b'zz', 24, 1, 1, date, t, 1, 101, 20000)
+# t_lst = [st, pt, it]
 
-for item in t_lst:
-    data = make_packet(item)
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((HOST, PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
 # sock.sendall(bytes(data + '\n', 'utf-8'))
-    sock.sendall(data)
+sock.sendall(packet)
 # recived = str(sock.recv(1024), 'utf-8')
-    recived = sock.recv(1024)
+recived = sock.recv(1024)
 
 
-    print('Отправлено:{}'.format(data))
-    print('Получено:  {}'.format(recived))
-    sock.close()
+print('Отправлено:{}'.format(packet))
+print('Получено:  {}'.format(recived))
+sock.close()
 
 # data = struct.unpack('2sh', recived)
 # print('Распаковано:', data)
--- lesson_2/hw_2/my_server.py
+++ lesson_2/hw_2/my_server.py
@@ -1,24 +1,55 @@
 import socketserver
+import datetime
 import struct
 from collections import namedtuple 
 
-ServiceTransaction = namedtuple('ServiceTransaction', ('''head, lenght,
-                                 term_id, transaction_id, date, time,
-                                 transaction_type, data'''))
+PACKET_HEAD = b'zz'
 
-PayTransaction = namedtuple('PayTransaction', ('''head, lenght,
-                                 term_id, transaction_id, date, time,
-                                 transaction_type, organization_id, summ'''))
+ServiceTransaction = namedtuple('ServiceTransaction', ('''lenght,
+                                 term_id, transaction_id, date, time_1,
+                                 time_2, time_3, transaction_type, data'''))
 
-InkassTransaction = namedtuple('InkassTransaction', ('''head, lenght,
-                                 term_id, transaction_id, date, time,
-                                 transaction_type, person_id, summ'''))
+PayTransaction = namedtuple('PayTransaction', ('''lenght,
+                                 term_id, transaction_id, date, time_1,
+                                 time_2, time_3, transaction_type,
+                                 id_, summ'''))
+
+
+def date_decode(input_str):
+    year = ((input_str & 0xfe00) >> 9)
+    year += 2000
+    # print(year)
+    month = ((input_str & 0x1e0) >> 5)
+    # print(month)
+    day = ((input_str & 0x1f))
+    # print(day)
+    return (year, month, day)
+
+
+def time_decode(first, second, third):
+    time = (first << 16) | (second << 8) | (third)
+    return time
+
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # self.data = self.request.recv(1024).decode()
         self.data = self.request.recv(1024)
         print('Клиент {} сообщает {}'.format(self.client_address[0], self.data))
+        s_struct = struct.Struct('BHIH3B2B')
+        un1 = ServiceTransaction._make(s_struct.unpack(self.data[2:]))
+        ud = date_decode(un1.date)
+        date = datetime.datetime(*ud)
+        ut = time_decode(un1.time_1, un1.time_2, un1.time_3)
+        time = datetime.timedelta(seconds=ut)
+        date = date + time
+        print('Длина пакета транзакции: {}'.format(un1.lenght))
+        print('Дата транзакции: {}'.format(date))
+        print('ID терминала: {}'.format(un1.term_id))
+        print('ID транзакции: {}'.format(un1.transaction_id))
+        print('Тип транзакции: {}'.format(un1.transaction_type))
+        print('Вид транзакции: {}'.format(un1.data))
+
         # self.request.sendall(bytes(self.data.upper(), 'utf-8'))
         self.request.sendall(self.data)
 
