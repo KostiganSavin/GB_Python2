490067e6b19ae490669b1608a6cc10d4b24a571d
Konsatntin Savin <kostiga.savin@gmail.com>
2017-04-17 14:11:37 +0300

    Убрал несколько пустых строк из db.py

--- lesson_3/hw3/db.py
+++ lesson_3/hw3/db.py
@@ -10,7 +10,6 @@ def make_db(clear=None):
             cursor.execute('drop table if exists credit;')
             cursor.execute('drop table if exists partner;')
             cursor.execute('drop table if exists payment;')
-        
         cursor.execute('''create table if not exists terminal (
                             id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                             configuration TEXT,
@@ -40,7 +39,6 @@ def make_db(clear=None):
                             summ INT,
                             FOREIGN KEY (agent_id) REFERENCES partner(id));
                        ''')
-       
         cursor.execute('''create table if not exists payment (
                             id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                             datetime TEXT,
